e272ab36ac5b2c8d1bff3277b13f36c0a2918bb2
Kerli Aava <52652273+kerliaava@users.noreply.github.com>
2020-01-27 18:53:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//select input and ul with the DOM
+
+let taskInput = document.getElementById("new-task");
+let addButton = document.getElementsByTagName("button")[0];
+let incompleteTaskHolder = document.getElementById("incomlpete-tasks");
+let completedTaskHolder = document.getElementById("completed-tasks");
